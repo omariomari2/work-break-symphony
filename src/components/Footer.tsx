0a646936e7db9b44cccd2f4330d7979420e1090c
Owusu Bright Omari <owusuomaribright@gmail.com>
2025-03-30 20:44:22 -0500

    updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
               break cycles effectively with customizable features.
             </p>
             <div className="flex space-x-4">
-              <a href="https://github.com/omariomari2/Pomodoro-Play.git" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-pomoprimary transition-colors">
+              <a href="https://github.com/omariomari2" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-pomoprimary transition-colors">
                 <Github size={20} />
               </a>
               <a href="https://x.com/omariii_vs" className="text-gray-500 hover:text-pomoprimary transition-colors">
